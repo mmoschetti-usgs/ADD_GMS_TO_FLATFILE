3300663923d52ba3a56df39fa07cb4b921078d5f
Moschetti <mmoschetti@igskcicgwsgm018.cr.usgs.gov>
2017-05-22 16:03:10 -0600

    misc changes

--- add_gms_to_flatfile.c
+++ add_gms_to_flatfile.c
@@ -14,12 +14,12 @@
 void delaz_(float *lat1, float *lon1, float *lat2, float *lon2, float *dist, float *az, float *baz);
 int getcols( const char * const line, const char * const delim, char ***out_storage);
 void strip(char *s);
-void assign_cols_flatfile(char **columns, float *stLat, float *stLon, float *evMag, float *evLon, float *evLat, float *evDep, int *evYear, int *evMon, int *evDay, int *evHour, int *evMin, float *evSec);
+void assign_cols_flatfile(char **columns, float *stLat, float *stLon, float *evMag, float *evLon, float *evLat, float *evDep, int *evYear, int *evMon, int *evDay, int *evHour, int *evMin, float *evSec, char *network, char * stationNm);
 char * replace(char const * const original, char const * const pattern, char const * const replacement );
 int compute_epochTime(int yearIn, int monthIn, int dayIn, int hourIn, int minIn, int secIn);
 
 /*--------------------------------------------------------------------------*/
-void assign_cols_flatfile(char **columns, float *stLat, float *stLon, float *evMag, float *evLon, float *evLat, float *evDep, int *evYear, int *evMon, int *evDay, int *evHour, int *evMin, float *evSec)
+void assign_cols_flatfile(char **columns, float *stLat, float *stLon, float *evMag, float *evLon, float *evLat, float *evDep, int *evYear, int *evMon, int *evDay, int *evHour, int *evMin, float *evSec, char *network, char *stationNm)
 /*--------------------------------------------------------------------------*/
 {
 //
@@ -35,6 +35,9 @@ void assign_cols_flatfile(char **columns, float *stLat, float *stLon, float *evM
   *evHour=atoi(columns[20]);
   *evMin=atoi(columns[21]);
   *evSec=atof(columns[22]);
+  network=strcpy(network,columns[1]);
+  stationNm=strcpy(stationNm,columns[2]);
+  fprintf(stderr,"assign_cols_flatfile, network/stationNm: %s %s\n", network, stationNm);
 
 }
 
@@ -70,6 +73,7 @@ int main (int argc, char *argv[])
   float dist, dist2, az, baz, mindist;
   char addToFlatFile[200], flatFile[200], flatFileMod[200];
   char buff[BUFFLEN], buff2[BUFFLEN], buffFin[BUFFLEN];
+  char network[5], stationNm[10], network2[5], stationNm2[10];
   char **columns;
   char delim[] = ",";
 
@@ -122,7 +126,9 @@ int main (int argc, char *argv[])
     columns = NULL;
     cols_found = getcols(buff, delim, &columns);
     if ( cols_found ) {
-      assign_cols_flatfile(columns, &stLat, &stLon, &evMag, &evLon, &evLat, &evDep, &evYear, &evMon, &evDay, &evHour, &evMin, &evSec);
+fprintf(stderr,"from %s\n",flatFile);
+fprintf(stderr,"%s\n",buff);
+      assign_cols_flatfile(columns, &stLat, &stLon, &evMag, &evLon, &evLat, &evDep, &evYear, &evMon, &evDay, &evHour, &evMin, &evSec, network, stationNm);
 //      for ( i = 0; i < cols_found; i++ ) printf("Column[ %d ] = %s\n", i, columns[ i ] ); 
       free(columns);
       epochTimeFlatFile=compute_epochTime(evYear,evMon,evDay,evHour,evMin,(int)evSec);
@@ -139,7 +145,9 @@ int main (int argc, char *argv[])
         strip(buff2);
         columns = NULL;
         cols_found = getcols(buff2, delim, &columns);
-        assign_cols_flatfile(columns, &stLat2, &stLon2, &evMag2, &evLon2, &evLat2, &evDep2, &evYear2, &evMon2, &evDay2, &evHour2, &evMin2, &evSec2);
+fprintf(stderr,"from %s",addToFlatFile);
+fprintf(stderr,"%s\n",buff2);
+        assign_cols_flatfile(columns, &stLat2, &stLon2, &evMag2, &evLon2, &evLat2, &evDep2, &evYear2, &evMon2, &evDay2, &evHour2, &evMin2, &evSec2, network2, stationNm2);
         free(columns);
         delaz_(&stLat,&stLon,&stLat2,&stLon2,&dist,&az,&baz);
         delaz_(&evLat,&evLon,&evLat2,&evLon2,&dist2,&az,&baz);
@@ -151,16 +159,21 @@ int main (int argc, char *argv[])
         if ( objMtmp < objMisfit ) {
           objMisfit=objMtmp;
           strcpy(buffFin,buff2);
+          fprintf(stderr,"objMisfit/dist/dist2/diffSec/diffMag: %.1f %.1f %.1f %.1f\n", objMisfit, dist, dist2, diffSec, diffMag);
 //          fprintf(stderr,"cnt1/cnt2: %d %d\n", cnt1, cnt2);
 //          fprintf(stderr,"%s\n%s\n", buff, buff2);
         }
+        else {
+          fprintf(stderr,"NO MATCH: objMisfit/dist/dist2/diffSec/diffMag: %.1f %.1f %.1f %.1f %s-%s (%s-%s)\n", objMisfit, dist, dist2, diffSec, diffMag, network2,stationNm2,network,stationNm);
+        }
 //        fprintf(stderr,"%s\n%s\n", buff, buffFin);
+exit(1);
       }
       fclose(fpAddToFlatFile);
     }
 // write modified flatfile
     if ( objMisfit < 25 ) {
-      fprintf(stderr,"%.3f %.3f %.3f %.3f %.2f %.2f\n", stLat, stLat2, stLon, stLon2, evMag, evMag2);
+      fprintf(stderr,"%.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.2f %.2f\n", stLat, stLat2, stLon, stLon2, evLat, evLat2, evLon, evLon2,evMag, evMag2);
       fprintf(stderr,"flatfile: %s\n", buff);
       fprintf(stderr,"modified flatfile %s\n", buffFin);
 exit(1);
@@ -174,6 +187,7 @@ exit(1);
       fprintf(stderr,"cnt1=%d\n", cnt1); 
     }
     cnt1++;
+exit(1);
   }
 
 
