e5d627723bf0aa7ee408466eb4b5b1783b0fa90b
Moschetti <mmoschetti@igskcicgltgm067.cr.usgs.gov>
2017-05-22 08:59:38 -0600

    first commit

---
+++ add_gms_to_flatfile.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include "params.h"
+
+
+/* Calculate distance and azimuth/back-azimuth between two (lon,lat) coordinates 
+ * using delaz.f subroutine
+ */
+
+// function declaration
+void delaz_(float *lat1, float *lon1, float *lat2, float *lon2, float *dist, float *az, float *baz);
+int getcols( const char * const line, const char * const delim, char ***out_storage);
+void strip(char *s);
+void assign_cols_flatfile(char **columns, float *stLat, float *stLon, float *evMag, float *evLon, float *evLat, float *evDep, int *evYear, int *evMon, int *evDay, int *evHour, int *evMin, float *evSec);
+char * replace(char const * const original, char const * const pattern, char const * const replacement );
+int compute_epochTime(int yearIn, int monthIn, int dayIn, int hourIn, int minIn, int secIn);
+
+/*--------------------------------------------------------------------------*/
+void assign_cols_flatfile(char **columns, float *stLat, float *stLon, float *evMag, float *evLon, float *evLat, float *evDep, int *evYear, int *evMon, int *evDay, int *evHour, int *evMin, float *evSec)
+/*--------------------------------------------------------------------------*/
+{
+//
+  *stLat=atof(columns[5]);
+  *stLon=atof(columns[6]);
+  *evMag=atof(columns[25]);
+  *evLon=atof(columns[27]);
+  *evLat=atof(columns[26]);
+  *evDep=atof(columns[28]);
+  *evYear=atoi(columns[17]);
+  *evMon=atoi(columns[18]);
+  *evDay=atoi(columns[19]);
+  *evHour=atoi(columns[20]);
+  *evMin=atoi(columns[21]);
+  *evSec=atof(columns[22]);
+
+}
+
+/*--------------------------------------------------------------------------*/
+void strip(char *s)
+/*--------------------------------------------------------------------------*/
+{
+    char *p2 = s;
+    while(*s != '\0') {
+        if(*s != '\t' && *s != '\n') {
+            *p2++ = *s++;
+        } else {
+            ++s;
+        }
+    }
+    *p2 = '\0';
+}
+
+/*--------------------------------------------------------------------------*/
+int main (int argc, char *argv[])
+/*--------------------------------------------------------------------------*/
+{
+  FILE *fpCat, *fpFlatFileMod, *fpFlatFile, *fpAddToFlatFile;
+  int cnt, cnt1, cnt2;
+  int hlines;
+  int cols_found, diffSec, minDiff;
+  int evYear, evMon, evDay, evHour, evMin;
+  int evYear2, evMon2, evDay2, evHour2, evMin2;
+  int epochTimeFlatFile, epochTimeFlatFile2; 
+  float objMisfit, objMtmp, diffMag;
+  float stLon, stLat, evMag, evLon, evLat, evDep, evSec;
+  float stLon2, stLat2, evMag2, evLon2, evLat2, evDep2, evSec2;
+  float dist, dist2, az, baz, mindist;
+  char addToFlatFile[200], flatFile[200], flatFileMod[200];
+  char buff[BUFFLEN], buff2[BUFFLEN], buffFin[BUFFLEN];
+  char **columns;
+  char delim[] = ",";
+
+/* CHECK INPUT ARGUMENTS */
+  if ( argc != 4 ) {
+    fprintf(stderr,"USAGE: %s [flatfile] [additional flatfile] [output flatfile]\n", argv[0]);
+    fprintf(stderr,"Relocated catalog file, e.g., emm_c2_OK_KS_201702_add_all2.csv\n");
+    exit(1);
+  }
+  sscanf(argv[1],"%s", flatFile);
+  sscanf(argv[2],"%s", addToFlatFile);
+  sscanf(argv[3],"%s", flatFileMod);
+
+// Open flatfiles
+  if ((fpFlatFile = fopen(flatFile, "r")) == NULL) {
+    fprintf(stderr,"Could not open flatfile, %s\n", flatFile);
+    exit(0);
+  }
+  else {
+    fprintf(stderr,"Opened flatfile, %s\n", flatFile);
+  }
+  if ((fpAddToFlatFile = fopen(addToFlatFile, "r")) == NULL) {
+    fprintf(stderr,"Could not open flatfile, %s\n", addToFlatFile);
+    exit(0);
+  }
+  else {
+    fprintf(stderr,"Opened add to flatfile, %s\n", addToFlatFile);
+  }
+  fclose(fpAddToFlatFile);
+  fpFlatFileMod = fopen(flatFileMod, "w");
+  fprintf(stderr,"Writing to flatfile, %s\n", flatFileMod);
+
+// READ/APPEND FLATFILE
+// header lines
+  hlines=2;
+  for (cnt1=0; cnt1<hlines; cnt1++) {
+    fgets(buff,BUFFLEN,fpFlatFile);
+//    strip(buff);
+    fprintf(fpFlatFileMod,"%s",buff);
+  }
+  cnt1=0;
+// loop over data values
+  while( fgets(buff,BUFFLEN,fpFlatFile) ) {
+    if ( strlen(buff) > BUFFLEN ) {
+      fprintf(stderr,"Increase BUFFLEN from %d.\n", (int)BUFFLEN);
+      exit(1);
+    }
+    strip(buff);
+//    fprintf(fpFlatFileMod,"%s,",buff);
+    columns = NULL;
+    cols_found = getcols(buff, delim, &columns);
+    if ( cols_found ) {
+      assign_cols_flatfile(columns, &stLat, &stLon, &evMag, &evLon, &evLat, &evDep, &evYear, &evMon, &evDay, &evHour, &evMin, &evSec);
+//      for ( i = 0; i < cols_found; i++ ) printf("Column[ %d ] = %s\n", i, columns[ i ] ); 
+      free(columns);
+      epochTimeFlatFile=compute_epochTime(evYear,evMon,evDay,evHour,evMin,(int)evSec);
+// loop through modified flatfile, add additional ground motions, as needed
+      cnt2=0;
+      minDiff=999;
+// loop through reprocessed ground motions
+      objMisfit=25;
+      fpAddToFlatFile = fopen(addToFlatFile, "r");
+// header lines
+      fgets(buff2,BUFFLEN,fpAddToFlatFile);
+      fgets(buff2,BUFFLEN,fpAddToFlatFile);
+      while( fgets(buff2,BUFFLEN,fpAddToFlatFile) ) {
+        strip(buff2);
+        columns = NULL;
+        cols_found = getcols(buff2, delim, &columns);
+        assign_cols_flatfile(columns, &stLat2, &stLon2, &evMag2, &evLon2, &evLat2, &evDep2, &evYear2, &evMon2, &evDay2, &evHour2, &evMin2, &evSec2);
+        free(columns);
+        delaz_(&stLat,&stLon,&stLat2,&stLon2,&dist,&az,&baz);
+        delaz_(&evLat,&evLon,&evLat2,&evLon2,&dist2,&az,&baz);
+//fprintf(stderr,"buff2: %s\n", buff2);
+        epochTimeFlatFile2=compute_epochTime(evYear2,evMon2,evDay2,evHour2,evMin2,(int)evSec2);
+        diffSec=abs(epochTimeFlatFile-epochTimeFlatFile2);
+        diffMag=fabsf(evMag-evMag2);
+        objMtmp=dist+dist2+(float)diffSec+diffMag*10;
+        if ( objMtmp < objMisfit ) {
+          objMisfit=objMtmp;
+          strcpy(buffFin,buff2);
+//          fprintf(stderr,"cnt1/cnt2: %d %d\n", cnt1, cnt2);
+//          fprintf(stderr,"%s\n%s\n", buff, buff2);
+        }
+//        fprintf(stderr,"%s\n%s\n", buff, buffFin);
+      }
+      fclose(fpAddToFlatFile);
+    }
+// write modified flatfile
+    if ( objMisfit < 25 ) {
+      fprintf(stderr,"%.3f %.3f %.3f %.3f %.2f %.2f\n", stLat, stLat2, stLon, stLon2, evMag, evMag2);
+      fprintf(stderr,"flatfile: %s\n", buff);
+      fprintf(stderr,"modified flatfile %s\n", buffFin);
+exit(1);
+      cnt2++;
+    }
+    else {
+      fprintf(fpFlatFileMod,"%s,",buff);
+    }
+    fprintf(fpFlatFileMod,"\n");
+    if ( cnt1%100 == 0 ) {
+      fprintf(stderr,"cnt1=%d\n", cnt1); 
+    }
+    cnt1++;
+  }
+
+
+
+  return 0;
+}
---
+++ backup/v20170430/read_line_fm_csvFile.c
@@ -0,0 +1,133 @@
+/* Tiny CSV Reader */
+/* Copyright (C) 2015, Deligiannidis Konstantinos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://w...content-available-to-author-only...u.org/licenses/>.  */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/* For more that 100 columns or lines (when delimiter = \n), minor modifications are needed. */
+int getcols( const char * const line, const char * const delim, char ***out_storage )
+
+{
+    const char *start_ptr, *end_ptr, *iter;
+    char **out;
+    int i;                                          //For "for" loops in the old c style.
+    int tokens_found = 1, delim_size, line_size;    //Calculate "line_size" indirectly, without strlen() call.
+    int start_idx[100], end_idx[100];   //Store the indexes of tokens. Example "Power;": loc('P')=1, loc(';')=6
+    //Change 100 with MAX_TOKENS or use malloc() for more than 100 tokens. Example: "b1;b2;b3;...;b200"
+
+    if ( *out_storage != NULL )                 return -4;  //This SHOULD be NULL: Not Already Allocated
+    if ( !line || !delim )                      return -1;  //NULL pointers Rejected Here
+    if ( (delim_size = strlen( delim )) == 0 )  return -2;  //Delimiter not provided
+
+    start_ptr = line;   //Start visiting input. We will distinguish tokens in a single pass, for good performance.
+                        //Then we are allocating one unified memory region & doing one memory copy.
+    while ( ( end_ptr = strstr( start_ptr, delim ) ) ) {
+
+        start_idx[ tokens_found -1 ] = start_ptr - line;    //Store the Index of current token
+        end_idx[ tokens_found - 1 ] = end_ptr - line;       //Store Index of first character that will be replaced with
+                                                            //'\0'. Example: "arg1||arg2||end" -> "arg1\0|arg2\0|end"
+        tokens_found++;                                     //Accumulate the count of tokens.
+        start_ptr = end_ptr + delim_size;                   //Set pointer to the next c-string within the line
+    }
+
+    for ( iter = start_ptr; (*iter!='\0') ; iter++ );
+
+    start_idx[ tokens_found -1 ] = start_ptr - line;    //Store the Index of current token: of last token here.
+    end_idx[ tokens_found -1 ] = iter - line;           //and the last element that will be replaced with \0
+
+    line_size = iter - line;    //Saving CPU cycles: Indirectly Count the size of *line without using strlen();
+
+    int size_ptr_region = (1 + tokens_found)*sizeof( char* );   //The size to store pointers to c-strings + 1 (*NULL).
+    out = (char**) malloc( size_ptr_region + ( line_size + 1 ) + 5 );   //Fit everything there...it is all memory.
+    //It reserves a contiguous space for both (char**) pointers AND string region. 5 Bytes for "Out of Range" tests.
+    *out_storage = out;     //Update the char** pointer of the caller function.
+
+    //"Out of Range" TEST. Verify that the extra reserved characters will not be changed. Assign Some Values.
+    //char *extra_chars = (char*) out + size_ptr_region + ( line_size + 1 );
+    //extra_chars[0] = 1; extra_chars[1] = 2; extra_chars[2] = 3; extra_chars[3] = 4; extra_chars[4] = 5;
+
+    for ( i = 0; i < tokens_found; i++ )    //Assign adresses first part of the allocated memory pointers that point to
+        out[ i ] = (char*) out + size_ptr_region + start_idx[ i ];  //the second part of the memory, reserved for Data.
+    out[ tokens_found ] = (char*) NULL; //[ ptr1, ptr2, ... , ptrN, (char*) NULL, ... ]: We just added the (char*) NULL.
+                                                        //Now assign the Data: c-strings. (\0 terminated strings):
+    char *str_region = (char*) out + size_ptr_region;   //Region inside allocated memory which contains the String Data.
+    memcpy( str_region, line, line_size );   //Copy input with delimiter characters: They will be replaced with \0.
+
+    //Now we should replace: "arg1||arg2||arg3" with "arg1\0|arg2\0|arg3". Don't worry for characters after '\0'
+    //They are not used in standard c lbraries.
+    for( i = 0; i < tokens_found; i++) str_region[ end_idx[ i ] ] = '\0';
+
+    //"Out of Range" TEST. Wait until Assigned Values are Printed back.
+    //for ( int i=0; i < 5; i++ ) printf("c=%x ", extra_chars[i] ); printf("\n");
+
+    // *out memory should now contain (example data):
+    //[ ptr1, ptr2,...,ptrN, (char*) NULL, "token1\0", "token2\0",...,"tokenN\0", 5 bytes for tests ]
+    //   |__________________________________^           ^              ^             ^
+    //          |_______________________________________|              |             |
+    //                   |_____________________________________________|      These 5 Bytes should be intact.
+
+    return tokens_found;
+}
+
+int read_line_from_csvFile(char *in_line,char **columns,int cols_found)
+
+{
+
+//    char in_line[] = "Arg1;;Th;s is not Del;m;ter;;Arg3;;;;Final";
+    char delim[] = ";;";
+//    char **columns;
+    int i;
+
+    printf("Example1:\n");
+    columns = NULL; //Should be NULL to indicate that it is not assigned to allocated memory. Otherwise return -4;
+
+    int cols_found = getcols( in_line, delim, &columns);
+    for ( i = 0; i < cols_found; i++ ) printf("Column[ %d ] = %s\n", i, columns[ i ] );  //<- (1st way).
+    // (2nd way) // for ( i = 0; columns[ i ]; i++) printf("start_idx[ %d ] = %s\n", i, columns[ i ] );
+
+    free( columns );    //Release the Single Contiguous Memory Space.
+    columns = NULL;     //Pointer = NULL to indicate it does not reserve space and that is ready for the next malloc().
+
+    printf("\n\nExample2, Nested:\n\n");
+
+    char example_file[] = "ID;Day;Month;Year;Telephone;email;Date of registration\n"
+            "1;Sunday;january;2009;123-124-456;jitter@go.xyz;2015-05-13\n"
+            "2;Monday;March;2011;(+30)333-22-55;buffer@wl.it;2009-05-23";
+
+    char **rows;
+    int j;
+
+    rows = NULL; //getcols() requires it to be NULL. (Avoid dangling pointers, leaks e.t.c).
+
+    getcols( example_file, "\n", &rows);
+    for ( i = 0; rows[ i ]; i++) {
+        {
+            printf("Line[ %d ] = %s\n", i, rows[ i ] );
+            char **columnX = NULL;
+            getcols( rows[ i ], ";", &columnX);
+            for ( j = 0; columnX[ j ]; j++) printf("  Col[ %d ] = %s\n", j, columnX[ j ] );
+            free( columnX );
+        }
+    }
+
+    free( rows );
+    rows = NULL;
+
+    return 0;
+}
